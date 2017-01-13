04b9d17e93d31d7e7547452cb8768deaf1f2a9fa
morrime <marymorrison15@gmail.com>
2017-01-13 13:40:13 -0800

    followers, following, and non-follow-back-ers working, rate limit is an issue. Currently using dummy account that doesn't have many followers/following

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,34 @@
 class HomeController < ApplicationController
   def show
     if !current_user.nil?
-      @followers = client.followers
-      @following = client.friends
-      @user = client.user
+
+
+      # @followers = client.followers
+      @followers = client.followers.take(200)
+      # begin
+      #   @followers.to_a
+      # rescue Twitter::Error::TooManyRequests => error
+      #   # NOTE: Your process could go to sleep for up to 15 minutes but if you
+      #   # retry any sooner, it will almost certainly fail with the same exception.
+      #   sleep error.rate_limit.reset_in + 1
+      #   retry
+      # end
+
+
+      # @following = client.friends
+      @following = client.friends.take(200)
+      # begin
+      #   @following.to_a
+      # rescue Twitter::Error::TooManyRequests => error
+      #   # NOTE: Your process could go to sleep for up to 15 minutes but if you
+      #   # retry any sooner, it will almost certainly fail with the same exception.
+      #   sleep error.rate_limit.reset_in + 1
+      #   retry
+      # end
+
+      @nonfollowers = @following.to_a - @followers.to_a
+      @your_non_followers = @followers.to_a - @following.to_a
+
     else
     end
     # @user = client.user(include_entities: true)
