da1ea869a1e1429924f9ce54941d4a0f712e6eef
morrime <marymorrison15@gmail.com>
2017-01-19 20:06:50 -0800

    working on fixing API vs Database loop.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,45 +4,51 @@ class HomeController < ApplicationController
 
   def show
     @now = Time.now.utc.to_s
+    @last_updated = Peep.all.order('updated_at DESC').first
     @last_update = Peep.all.order('updated_at DESC').first.updated_at.to_s
     @time_diff = Time.diff(Time.parse(@last_update), Time.parse(@now))
 
     # if user is signed in
     if !current_user.nil?
       # if it's been longer than 12 or more hours since last API call
-      if @time_diff[:hour] >= 0 || @time_diff[:day] > 0 || @time_diff[:week] > 0 || @time_diff[:month] > 0 || @time_diff[:year] > 0
+      if @time_diff[:minute] >= 10 || @time_diff[:hour] >= 1 || @time_diff[:day] >= 1 || @time_diff[:week] >= 1 || @time_diff[:month] >= 1 || @time_diff[:year] >= 1
 
         # API FOLLOWERS:
         @followers_objects = client.followers
+        @followers_objects_clone = @followers_objects.dup
         @followers = []
-        @followers_objects.each do |peep|
+        @followers_objects_clone.each do |peep|
           @followers << peep.screen_name
         end
         # @followers = client.followers.take(200)
-        @followers_objects.each do |api_data|
+        @followers_objects_clone.each do |api_data|
           peep = Peep.find_or_create_by(api_data.screen_name, api_data.id)
           Follower.find_or_create_by(current_user.id, peep.id)
         end
 
         # API FOLLOWEES:
         @followees_objects = client.friends
+        @followees_objects_clone = @followees_objects.dup
         @followees = []
-        @followees_objects.each do |peep|
+        @followees_objects_clone.each do |peep|
           @followees << peep.screen_name
         end
 
         # @followees = client.friends.take(200)
-        @followees_objects.each do |api_data|
+        @followees_objects_clone.each do |api_data|
           peep = Peep.find_or_create_by(api_data.screen_name, api_data.id)
           Followee.find_or_create_by(current_user.id, peep.id)
         end
 
         @nonfollowers = @followees.to_a - @followers.to_a
         @your_non_followers = @followers.to_a - @followees.to_a
+        @nonfollowers << "API"
+
       # if it hasn't been longer than 12+ hrs since last API call
       else
         # Get info for current_user from DB
         user = User.find_by(name: current_user.name)
+
         # DATABASE FOLLOWERS
         user_follower_objects = Follower.where(user_id: user.id)
         @user_followers_array = []
@@ -63,6 +69,7 @@ class HomeController < ApplicationController
 
         @nonfollowers = @followees.to_a - @followers.to_a
         @your_non_followers = @followers.to_a - @followees.to_a
+        @nonfollowers << "DATABASE"
       #
       end
     end
