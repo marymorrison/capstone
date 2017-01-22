ba52ebb45e52bb5d982ea7d1776a161395cfedfc
morrime <marymorrison15@gmail.com>
2017-01-22 00:08:57 -0800

    added comments to home_controller and refactored a little bit. NEED MORE REFACTORING

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,30 +4,33 @@ class HomeController < ApplicationController
   require 'date'
 
   def show
-    # if user is signed in
+    # USER IS SIGNED IN
     if !current_user.nil?
-# //////////////////////////////////////////////////////////////////////
-# //////////////////////////////////////////////////////////////////////
 
+      # ESTABLISH CURRENT TIME
       @now = Time.now.utc.to_s
-      # Get most recent Follower instance from DB and convert to string
+
+      # GET MOST RECENT FOLLOWER UPDATE TIME FROM DATABASE & CONVERT TO STRING
       if Follower.all.count > 0
         @follower_last_update = Follower.all.order('updated_at DESC').first.updated_at.to_s
-        # create hash with key datetime, and value of most recent Follower, parsed by date gem
+
+        # CREATE HASH W/ KEY: DATETIME & VALUE: MOST RECENT FOLLOWER UPDATE TIME, PARSED BY DATE GEM
         @follower = {}
         @follower[:datetime_parsed] = DateTime.parse(@follower_last_update)
         @follower[:time_diff_string] = @follower_last_update
       end
-      # Get most recent Followee instance from DB and convert to string
+
+      # GET MOST RECENT FOLLOWEE UPDATE TIME FROM DATABASE & CONVERT TO STRING
       if Followee.all.count > 0
         @followee_last_update = Followee.all.order('updated_at DESC').first.updated_at.to_s
-        # create hash with key datetime, and value of most recent Followee, parsed by date gem
+        # CREATE HASH W/ KEY: DATETIME & VALUE: MOST RECENT FOLLOWEE UPDATE TIME, PARSED BY DATE GEM
         @followee = {}
         @followee[:datetime_parsed] = DateTime.parse(@followee_last_update)
         @followee[:time_diff_string] = @followee_last_update
-        # put parsed & most recent Followee and Follower into array and sort on datetime field to find most recent update
       end
-      # this if statement and previous 2 will account for any absence of data so no errors occur
+
+      # PUT PARSED & MOST RECENT FOLLOWEE & FOLLOWER INTO ARRAY AND SORT ON DATETIME FIELD TO FIND MOST RECENT UPDATE
+      # HANDLES ABSENCE OF DATA USED TO DETERMINE USE OF API OR DATABASE
       if Followee.all.count > 0 && Follower.all.count > 0
         @list = [@followee, @follower]
         @most_recent_update_time = @list.sort do |a, b|
@@ -58,8 +61,8 @@ class HomeController < ApplicationController
         @time_diff[:year] = 1
       end
 
-      # if it's been longer than 12 or more hours since last API call
-      if (@time_diff[:minute] >= 1 || @time_diff[:hour] >= 1 || @time_diff[:day] >= 1 || @time_diff[:week] >= 1 || @time_diff[:month] >= 1 || @time_diff[:year] >= 1) 
+      # TO DETERMINE WHETHER TO CALL API OR USE DATABASE
+      if (@time_diff[:minute] >= 1 || @time_diff[:hour] >= 1 || @time_diff[:day] >= 1 || @time_diff[:week] >= 1 || @time_diff[:month] >= 1 || @time_diff[:year] >= 1)
 
         # API FOLLOWERS:
         @followers = []
@@ -82,39 +85,13 @@ class HomeController < ApplicationController
           peep = Peep.find_or_create_by(api_data.screen_name, api_data.id, api_data.profile_image_url_https)
           Followee.find_or_create_by(current_user.id, peep.id)
         end
-
-        # DETERMINE RUDE PPL AND GROUPIES
-        @nonfollowers = @followees.to_a - @followers.to_a
-        @nonfollowers_images = @followees_images.to_a - @followers_images.to_a
-        @your_non_followers = @followers.to_a - @followees.to_a
-        @your_non_followers_images = @followers_images.to_a - @followees_images.to_a
-
-        @nonfollowers_array_of_arrays = []
-        if @nonfollowers_images.length != nil
-          @nonfollowers_images.length.times do |i|
-            array = []
-            array << @nonfollowers[i]
-            array << @nonfollowers_images[i]
-            @nonfollowers_array_of_arrays << array
-          end
-        end
-
-        @your_non_followers_array_of_arrays = []
-        if @your_non_followers_images.length != nil
-          @your_non_followers_images.length.times do |i|
-            array = []
-            array << @your_non_followers[i]
-            array << @your_non_followers_images[i]
-            @your_non_followers_array_of_arrays << array
-          end
-        end
-
-        # TO KNOW WHERE DATA IS COMING FROM
+        # WHERE DATA IS COMING FROM
         @data_origin = "API"
 
-      # if it hasn't been longer than 12+ hrs since last API call
       else
-        # Get info for current_user from DB
+      # DATABASE
+      # USER IS NOT NEW & IF API HAS BEEN CALLED RECENTLY
+
         user = User.find_by(name: current_user.name)
 
         # DATABASE FOLLOWERS
@@ -137,42 +114,43 @@ class HomeController < ApplicationController
           @followees_images << peep_obj.image_url
         end
 
-        # DETERMINE RUDE PPL AND GROUPIES
-        @nonfollowers = @followees.to_a - @followers.to_a
-        @nonfollowers_images = @followees_images.to_a - @followers_images.to_a
-        @your_non_followers = @followers.to_a - @followees.to_a
-        @your_non_followers_images = @followers_images.to_a - @followees_images.to_a
-
-        @nonfollowers_array_of_arrays = []
-        if @nonfollowers_images.length != nil
-          @nonfollowers_images.length.times do |i|
-            array = []
-            array << @nonfollowers[i]
-            array << @nonfollowers_images[i]
-            @nonfollowers_array_of_arrays << array
-          end
-        end
-
-        @your_non_followers_array_of_arrays = []
-        if @your_non_followers_images.length != nil
-          @your_non_followers_images.length.times do |i|
-            array = []
-            array << @your_non_followers[i]
-            array << @your_non_followers_images[i]
-            @your_non_followers_array_of_arrays << array
-          end
-        end
-        # TO KNOW WHERE DATA IS COMING FROM
+        # WHERE DATA IS COMING FROM
         @data_origin = "DATABASE"
       end
-        @current_user_image ||= User.find(session[:user_id]).image_url if session[:user_id]
+
+      # DETERMINE RUDE PPL & GROUPIES
+      @nonfollowers = @followees.to_a - @followers.to_a
+      @nonfollowers_images = @followees_images.to_a - @followers_images.to_a
+      @your_non_followers = @followers.to_a - @followees.to_a
+      @your_non_followers_images = @followers_images.to_a - @followees_images.to_a
+
+      @nonfollowers_array_of_arrays = []
+      if @nonfollowers_images.length != nil
+        @nonfollowers_images.length.times do |i|
+          array = []
+          array << @nonfollowers[i]
+          array << @nonfollowers_images[i]
+          @nonfollowers_array_of_arrays << array
+        end
+      end
+
+      @your_non_followers_array_of_arrays = []
+      if @your_non_followers_images.length != nil
+        @your_non_followers_images.length.times do |i|
+          array = []
+          array << @your_non_followers[i]
+          array << @your_non_followers_images[i]
+          @your_non_followers_array_of_arrays << array
+        end
+      end
+      @current_user_image ||= User.find(session[:user_id]).image_url if session[:user_id]
 
     end
   end
 
   def unfollow
       client.unfollow(params[:peep_name])
-      # Delete nonfollower from DB so update shows immediately
+      # DELETE NONFOLLOWER FROM DB SO UPDATES IMMEDIATELY
       peep = Peep.find_by(name: params[:peep_name])
       if Followee.find_by(peep_id: peep.id, user_id: current_user.id) != nil
         Followee.find_by(peep_id: peep.id, user_id: current_user.id).destroy
@@ -182,7 +160,7 @@ class HomeController < ApplicationController
 
   def follow
     client.follow(params[:peep_name])
-  #   Delete 'follower' from DB so update shows immediately
+    # DELETE FOLLOWER FROM DB SO UPDATES IMMEDIATELY
     peep = Peep.find_by(name: params[:peep_name])
     if Follower.find_by(peep_id: peep.id, user_id: current_user.id) != nil
       Follower.find_by(peep_id: peep.id, user_id: current_user.id).destroy
